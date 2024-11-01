3778b9ec6dbe4195993d726cdd493f746dd38812
Verkkel <64005012+Taloonys@users.noreply.github.com>
2024-11-01 13:00:24 +0300

    chore(functions.rs): in-github minor changes

--- rust/src/functions.rs
+++ rust/src/functions.rs
@@ -1,3 +1,6 @@
+/*
+ * Btw, there is not void-like type, if function doesn't return anything, it's `->` is ommited
+ */
 fn summ(x: i32, y: i32) -> i32 { x + y }
 
 
@@ -7,7 +10,9 @@ fn form_card(name: &str, age: i32) {
 }
 
 
-fn in_out_square(mut x: i32) { x = x^2; }   // didn't tested
+fn mut_input(mut x: i32) {                  // `mut` word
+    x = x * 10;                             // for changing `x` inside function scope
+}
 
 
 fn code_block() {
@@ -20,4 +25,15 @@ fn code_block() {
     block;                                  // ok
     // block();                             /*<-- err, not like this */
     println!("{block}");                    // will print return value -> `999`
+}
+
+
+fn func_as_value() {
+    let f_summ: fn(i32, i32) -> i32 = summ;
+    println!("f_summ = {}", f_summ(3, 3));
+}
+
+
+fn func_as_input(op: fn(i32, i32) -> i32) {        // same for return case...
+    println!("func_input_summ = {}", op(3, 3));    // assume to pass summ func, for example
 }
