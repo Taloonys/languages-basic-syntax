7dee26442cc6faa33b8097362a6a0fdd60a238d2
Taloonys <vxcvxcv200071@gmail.com>
2024-11-17 15:50:15 +0300

    chore(rust): dropped cargo

--- rust/README.md
+++
@@ -1,9 +0,0 @@
-# Rust
-* static typing, compile language
-* easy to work with C/C++ code through `FFI` 
-* `rustc` is for compiling
-* `cargo` is `build system` + `packet manager`
-
-# Notes
-* super comfortable cargo made me to use it inside `syntax notes`, but it prb would be deleted
-* i'll use `fn main()` for every single `.rs` file
---
+++ rust/main.rs
@@ -0,0 +1,25 @@
+/**
+ * `#[test]` attribute in rust usually search tests inside main, this attribute will trigger on `cargo test`
+ * Ofc tests should be in a different module and there should be used `#[cfg(test)]`
+ * 
+ * I left only simple use case, because unit-testing is connected with cargo setup
+ */
+
+
+fn summ(a: i32, b: i32) -> i32 { a + b }
+
+
+#[test]
+fn test_summ() {
+    let a = 10;
+    let b = 50;
+
+    let result = summ(a, b);
+
+    assert_eq!(result, 60, "Err Summ");
+}
+
+
+fn main() {
+    summ(1, 3);
+}
